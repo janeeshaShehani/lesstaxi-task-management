3e68f7d88dbfaade5577a627725314913cd61ba6
janeeshaShehani <148691632+janeeshaShehani@users.noreply.github.com>
2026-09-10 16:17:01 +0530

    Fix production CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://lesstaxi-task-management.vercel.app",
+    ],
   })
 );
 
